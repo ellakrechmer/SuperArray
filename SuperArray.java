58a3ae9c79cc59a5d804adfb00df345b18b933ee
ellakrechmer <ekrechmer20@stuy.edu>
2020-10-30 15:20:50 -0400

    Created and tested constructor; added getData() method to help with testing

--- SuperArray.java
+++ SuperArray.java
@@ -1,4 +1,10 @@
 public class SuperArray {
-  private String [] data;
-  private size;
+  private String[] data;
+  private int size;
+  public SuperArray(){
+    data = new String[10];
+  }
+  public String[] getData(){
+    return data;
+  }
 }
--- Tester.java
+++ Tester.java
@@ -1,5 +1,7 @@
+import java.util.Arrays;
 public class Tester{
   public static void main(String[] args){
-    
+    SuperArray A=new SuperArray();
+    System.out.println(Arrays.toString(A.getData()));
   }
 }
